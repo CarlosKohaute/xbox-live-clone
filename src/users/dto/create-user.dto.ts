ac430c3e5b28f1d687f8d948ea99e279fb9790c7
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-06-27 18:21:20 -0300

    Commit validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 export class CreateUserDto {
-  @IsNotEmpty()
   @IsString()
   name: string;
 
   user: string;
 
-  @IsNotEmpty()
   @IsEmail()
-  @IsString()
   email: string;
 }
