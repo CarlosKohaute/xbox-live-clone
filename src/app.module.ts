2a62415243db9a33c0331b017da2e04dcc6c9d1c
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-07-18 21:47:38 -0300

    created the dto and the entities of orders

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { GenreModule } from './genre/genre.module';
 import { CategoriesModule } from './categories/categories.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, GenreModule, CategoriesModule],
+  imports: [UsersModule, ProductsModule, GenreModule, CategoriesModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Profiles')
+@Controller('orders')
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: 'Criar um novo perfil',
+  })
+  create(@Body() dto: CreateOrderDto) {
+    return this.ordersService.create(dto);
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: 'Buscar todos os perfis',
+  })
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Buscar um perfil por id',
+  })
+  findOne(@Param('id') id: string) {
+    return this.ordersService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Atualizar um perfil',
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateOrderDto) {
+    return this.ordersService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({
+    summary: 'Deletar um perfil',
+  })
+  remove(@Param('id') id: string) {
+    return this.ordersService.remove(id);
+  }
+}
